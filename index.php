81ada000df7d74dda4bf2a3210d5b2f7769e0b56
easygode <bimalbenvenuti@gmail.com>
2023-04-17 12:45:35 +0200

    membership

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
 require_once __DIR__ . '/Models/User.php';
+require_once __DIR__ . '/Models/Membership.php';
 require_once __DIR__ . '/Models/PremiumUser.php';
 
-$gaetano = new User();
-$gaspare = new PremiumUser ();
+$gold_membership = new Membership()
+
+$gaetano = new User('Gaetano' , 39);
+$gaspare = new PremiumUser ('Gaspare' , 30);
 
 class User
 {
@@ -44,5 +47,3 @@ $user = new User('Gaetano');
 
 $employee = new Employee('Ivan', 1);
 //$employee->set_level(1);
-
-$little_user = new LittleUser()
